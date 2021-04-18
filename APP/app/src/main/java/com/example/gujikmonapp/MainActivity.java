b3eeaca0fa1eec65bdf1d6766402325bd6b0cca5
kssgit <tjdtnmail@gmail.com>
2021-04-19 00:01:53 +0900

    [ADD]model and activity

--- APP/app/src/main/java/com/example/gujikmonapp/MainActivity.java
+++ APP/app/src/main/java/com/example/gujikmonapp/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.gujikmonapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    View  companySearch;
+    ImageButton filter , cofavorites, searchBtn;
+    EditText coNm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+//       검색 바
+        companySearch = findViewById(R.id.search_bar); // SearchView
+//        버튼
+        filter = findViewById(R.id.filter);
+        cofavorites = findViewById(R.id.cofavorites);
+        searchBtn = findViewById(R.id.search);
+//        검색한 기업 명
+        coNm = findViewById(R.id.coNm);
+    }//onCreate()
+
+
+
+
+
+
 }
---
+++ APP/app/src/main/java/com/example/gujikmonapp/companyInfo.java
@@ -0,0 +1,162 @@
+package com.example.gujikmonapp;
+
+import java.sql.Array;
+
+public class companyInfo {
+    private String coNm,busiNo,coaddr,suoerRegionNm,regionNm,x,y,superIndTpCd,superIndTpNm,indTpCd,indTpNm,coMainProd,coHomePage,alwaysWorkerCnt;
+    private int superRegionCd,regionCd;
+    private  Boolean recruitment;
+    private Array sgBrandNm,info;
+
+    public String getCoNm() {
+        return coNm;
+    }
+
+    public void setCoNm(String coNm) {
+        this.coNm = coNm;
+    }
+
+    public String getBusiNo() {
+        return busiNo;
+    }
+
+    public void setBusiNo(String busiNo) {
+        this.busiNo = busiNo;
+    }
+
+    public String getCoaddr() {
+        return coaddr;
+    }
+
+    public void setCoaddr(String coaddr) {
+        this.coaddr = coaddr;
+    }
+
+    public String getSuoerRegionNm() {
+        return suoerRegionNm;
+    }
+
+    public void setSuoerRegionNm(String suoerRegionNm) {
+        this.suoerRegionNm = suoerRegionNm;
+    }
+
+    public String getRegionNm() {
+        return regionNm;
+    }
+
+    public void setRegionNm(String regionNm) {
+        this.regionNm = regionNm;
+    }
+
+    public String getX() {
+        return x;
+    }
+
+    public void setX(String x) {
+        this.x = x;
+    }
+
+    public String getY() {
+        return y;
+    }
+
+    public void setY(String y) {
+        this.y = y;
+    }
+
+    public String getSuperIndTpCd() {
+        return superIndTpCd;
+    }
+
+    public void setSuperIndTpCd(String superIndTpCd) {
+        this.superIndTpCd = superIndTpCd;
+    }
+
+    public String getSuperIndTpNm() {
+        return superIndTpNm;
+    }
+
+    public void setSuperIndTpNm(String superIndTpNm) {
+        this.superIndTpNm = superIndTpNm;
+    }
+
+    public String getIndTpCd() {
+        return indTpCd;
+    }
+
+    public void setIndTpCd(String indTpCd) {
+        this.indTpCd = indTpCd;
+    }
+
+    public String getIndTpNm() {
+        return indTpNm;
+    }
+
+    public void setIndTpNm(String indTpNm) {
+        this.indTpNm = indTpNm;
+    }
+
+    public String getCoMainProd() {
+        return coMainProd;
+    }
+
+    public void setCoMainProd(String coMainProd) {
+        this.coMainProd = coMainProd;
+    }
+
+    public String getCoHomePage() {
+        return coHomePage;
+    }
+
+    public void setCoHomePage(String coHomePage) {
+        this.coHomePage = coHomePage;
+    }
+
+    public String getAlwaysWorkerCnt() {
+        return alwaysWorkerCnt;
+    }
+
+    public void setAlwaysWorkerCnt(String alwaysWorkerCnt) {
+        this.alwaysWorkerCnt = alwaysWorkerCnt;
+    }
+
+    public int getSuperRegionCd() {
+        return superRegionCd;
+    }
+
+    public void setSuperRegionCd(int superRegionCd) {
+        this.superRegionCd = superRegionCd;
+    }
+
+    public int getRegionCd() {
+        return regionCd;
+    }
+
+    public void setRegionCd(int regionCd) {
+        this.regionCd = regionCd;
+    }
+
+    public Boolean getRecruitment() {
+        return recruitment;
+    }
+
+    public void setRecruitment(Boolean recruitment) {
+        this.recruitment = recruitment;
+    }
+
+    public Array getSgBrandNm() {
+        return sgBrandNm;
+    }
+
+    public void setSgBrandNm(Array sgBrandNm) {
+        this.sgBrandNm = sgBrandNm;
+    }
+
+    public Array getInfo() {
+        return info;
+    }
+
+    public void setInfo(Array info) {
+        this.info = info;
+    }
+}
